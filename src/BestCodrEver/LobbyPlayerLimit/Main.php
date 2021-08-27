16613ba9b09914b4a2244335836367aab46eb9b9
BestCodrEver <70841501+BestCodrEver@users.noreply.github.com>
2021-08-27 21:09:35 +1000

    Fixes: - Fixed error being returned if the correct values were inserted - Fixed task scheduler attempting to run when the config values are wrong

--- src/BestCodrEver/LobbyPlayerLimit/Main.php
+++ src/BestCodrEver/LobbyPlayerLimit/Main.php
@@ -23,16 +23,15 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\level\Level;
 
-
 class Main extends PluginBase implements Listener{
   public $lobbyConfig;
     public function onEnable(){
       $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveResource("lobbies.yml");
         $this->lobbyConfig = new Config($this->getDataFolder() . "lobbies.yml", Config::YAML);
-        if ($this->getLimit() < 0 || is_null($this->getMsg()) || count($this->getLobbies()) === 0 || is_null($this->getFallback()) || Server::getInstance()->isLevelGenerated($this->getFallback())){
+        if ($this->getLimit() < 0 || is_null($this->getMsg()) || count($this->getLobbies()) === 0 || is_null($this->getFallback()) || !Server::getInstance()->isLevelGenerated($this->getFallback())){
           $this->error();
-          return;
+		      return;
         }
         
         $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(
@@ -61,15 +60,15 @@ class Main extends PluginBase implements Listener{
         $fallback = $this->getFallback();
         $this->getServer()->loadLevel($fallback);
         foreach ($lobbies as $lobby) {
-          if(Server::getInstance()->isLevelGenerated($lobby)){
+		      if(!Server::getInstance()->isLevelGenerated($lobby)){
             $this->error();
+			      return true;          
+          }
+          $this->getServer()->loadLevel($lobby);
+          if (count($this->getServer()->getLevelByName($lobby)->getPlayers()) < $this->getLimit()){
+            $sender->teleport($this->getServer()->getLevelByName($lobby)->getSafeSpawn());
             return true;
           }
-            $this->getServer()->loadLevel($lobby);
-            if (count($this->getServer()->getLevelByName($lobby)->getPlayers()) < $this->getLimit()){
-                $sender->teleport($this->getServer()->getLevelByName($lobby)->getSafeSpawn());
-                return true;
-            }
         }
         $sender->teleport($this->getServer()->getLevelByName($fallback)->getSafeSpawn());
         $sender->sendMessage($this->getMsg());
